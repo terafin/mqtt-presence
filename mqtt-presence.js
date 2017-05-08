851714f73d319204638657b15ced40b6bff4a10d
Justin Wood <woody@apple.com>
2017-05-07 21:53:29 -0700

    working presence

--- mqtt-presence.js
+++ mqtt-presence.js
@@ -1,6 +1,9 @@
 // Requirements
 const url = require('url')
 const express = require('express')
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
 const mqtt = require('mqtt')
 const logging = require('./homeautomation-js-lib/logging.js')
 require('./homeautomation-js-lib/mqtt_helpers.js')
@@ -34,21 +37,25 @@ client.on('message', (topic, message) => {
 
 // HS Web API
 const app = express()
+app.use(bodyParser.json())
 
-app.get('/geofence/*', function(req, res) {
+app.post('/geofence/*', function(req, res) {
     const url_info = url.parse(req.url, true)
     var topic = url_info.pathname
-    var value = url_info.query.value
+    const body = req.body
+    const locationName = body.name
+    const value = body.entry
+    const components = topic.split('/')
+    const person = components[components.length - 1]
+    topic = topic_prefix + '/geofence/' + locationName.toLowerCase() + '/' + person
 
-    topic = topic_prefix + topic
+    logging.debug('body: ' + JSON.stringify(body))
 
-
-    logging.log('publishing geofence status', {
+    logging.info('publishing geofence status', Object.assign(body, {
         action: 'geofence-update',
-        name: name,
         topic: topic,
         value: value
-    })
+    }))
     client.publish(topic, value)
     res.send('topic: ' + topic + ' value: ' + value)
 })
